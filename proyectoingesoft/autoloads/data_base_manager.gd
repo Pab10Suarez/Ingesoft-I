b3f492403bc38541b4997dfb833d8e809dea608c
emiliano <68301161+soetgdeznsgk@users.noreply.github.com>
2025-06-16 12:03:49 -0500

    ADD: Escenas para julián y sergio

---
+++ proyectoingesoft/autoloads/data_base_manager.gd
@@ -0,0 +1,12 @@
+extends Node
+
+# Éste script es un "autoload", el equivalente en Godot de los Singletons ( https://www.youtube.com/watch?v=09-aceKznw8 )
+
+# Éste script debería tener las funciones a llamar para subir datos a la base de datos (como no hemos hecho todavía el formato de guardado de juego,
+# puedes utilizar el tipado dinámico de GDScript para hacer que funcionen.
+
+# Con F1 puedes acceder a la documentación desde el editor
+func _ready() -> void:
+	# Acá puedes testear el código, cuando le des "ejecutar", ésta función se llamará automáticamente (así la pantalla esté gris)
+	# Un buen recurso para entender ésto es https://kidscancode.org/godot_recipes/3.x/basics/tree_ready_order/index.html
+	pass
---
+++ proyectoingesoft/scenes/game/characters/player/player.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+class_name Player
+
+# Éste script hace parte de un "nodo", el nodo tiene tipo CharacterBody2D, si pones F1 puedes ver la documentación
+# Para probar el movimiento del jugador, puedes darle a F6 y ejecutar ésta escena aislada del resto del juego
+
+func _ready() -> void:
+	# Ésta función se llama cuando el "nodo" entra a la escena
+	# Un buen recurso para entender ésta funcion es https://kidscancode.org/godot_recipes/3.x/basics/tree_ready_order/index.html
+	pass
+
+func _physics_process(delta: float) -> void:
+	# Ésta función se llama cada frame de física, es ideal para indicar movimiento ya que no depende del framerate del juego en un PC en específico
+	# Un buen recurso para entender ésta función es https://www.reddit.com/r/godot/comments/bhptsm/process_physics_process_and_physics_frames/
+	pass
+	
