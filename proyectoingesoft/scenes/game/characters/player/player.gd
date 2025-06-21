96cf9d4c3a2474ac4a40807adec0257cc66b9024
chexotv77 <136018416+chexotv77@users.noreply.github.com>
2025-06-21 15:16:45 -0500

    Update player.gd
    script movimiento del jughador con flechas del teclado

--- proyectoingesoft/scenes/game/characters/player/player.gd
+++ proyectoingesoft/scenes/game/characters/player/player.gd
@@ -1,17 +1,19 @@
 extends CharacterBody2D
-class_name Player
 
-# Éste script hace parte de un "nodo", el nodo tiene tipo CharacterBody2D, si pones F1 puedes ver la documentación
-# Para probar el movimiento del jugador, puedes darle a F6 y ejecutar ésta escena aislada del resto del juego
+const SPEED = 200  # Velocidad del jugador
 
-func _ready() -> void:
-	# Ésta función se llama cuando el "nodo" entra a la escena
-	# Un buen recurso para entender ésta funcion es https://kidscancode.org/godot_recipes/3.x/basics/tree_ready_order/index.html
-	pass
+func _physics_process(delta):
+	var direction = Vector2.ZERO
 
-func _physics_process(delta: float) -> void:
-	# Ésta función se llama cada frame de física, es ideal para indicar movimiento ya que no depende del framerate del juego en un PC en específico
-	# Para tratar los inputs del teclado, puedes utilizar los InputEvents que configuré (Proyecto -> Configuración del proyecto -> Mapa de entrada)
-	# Un buen recurso para entender ésta función es https://www.reddit.com/r/godot/comments/bhptsm/process_physics_process_and_physics_frames/
-	pass
-	
+	if Input.is_action_pressed("ui_right"):
+		direction.x += 1
+	if Input.is_action_pressed("ui_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		direction.y += 1
+	if Input.is_action_pressed("ui_up"):
+		direction.y -= 1
+
+	direction = direction.normalized()
+	velocity = direction * SPEED
+	move_and_slide()
