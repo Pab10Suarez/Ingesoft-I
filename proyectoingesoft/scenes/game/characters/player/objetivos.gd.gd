78d679a5b5a9f13fe7e9377053fb15607abdab05
julian-2348 <jmurilloro@unal.edu.co>
2025-07-15 18:54:18 -0500

    Delete proyectoingesoft/scenes/game/characters/player/objetivos.gd.gd

--- proyectoingesoft/scenes/game/characters/player/objetivos.gd.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-enum Objetivos {
-	EXPLORAR,  # El personaje está explorando el mundo
-	SEGUIR,    # El personaje sigue a otro
-	JUGAR,     # El personaje está jugando
-	PROTEGER   # El personaje está protegiendo a alguien
-}
