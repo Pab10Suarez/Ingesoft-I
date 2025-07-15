b143053d81a34fb1631b7d64b52251abad55bd8f
julian-2348 <jmurilloro@unal.edu.co>
2025-07-15 18:53:55 -0500

    Delete proyectoingesoft/scenes/game/characters/player/gerardo.tscn.gd

--- proyectoingesoft/scenes/game/characters/player/gerardo.tscn.gd
+++
@@ -1,6 +0,0 @@
-# gerardo.tscn
-extends "res://maquina_estado.gd"  # Extiende de la máquina de estados
-
-func _ready():
-	# Inicializa el estado de Gerardo
-	set_estado(EstadoPersonaje.IDLE)  # Gerardo comienza en estado inactivo
