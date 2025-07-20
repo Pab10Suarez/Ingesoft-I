13bc3eadd412f3ec29ba1fd0d971f13fdbb6fada
julian-2348 <jmurilloro@unal.edu.co>
2025-07-20 03:46:29 -0500

    Update guillermo.gd

--- proyectoingesoft/scenes/game/characters/children/claudio/claudio.gd
+++ proyectoingesoft/scenes/game/characters/children/claudio/claudio.gd
@@ -1,6 +1,42 @@
-# claudiot_tscn.gd
-extends "res://maquina_estado.gd"
+extends CharacterBody2D
+class_name Guillermo
 
-func _ready():
-	# Asignar el estado inicial
-	set_estado(EstadoPersonaje.IDLE)
+const VELOCIDAD_CAMINAR := 30.0
+const DISTANCIA_MINIMA_DE_GERARDO := 130.0
+
+enum EstadoGuillermo {
+    NEUTRAL,
+    SUFRIENDO
+}
+
+var estado_actual: EstadoGuillermo = EstadoGuillermo.NEUTRAL
+@onready var gerardo := get_tree().get_first_node_in_group("Player")
+@onready var sprite := $AnimatedSprite2D
+
+func _ready() -> void:
+    cambiar_estado(EstadoGuillermo.NEUTRAL)
+
+func _physics_process(delta: float) -> void:
+    if gerardo:
+        seguir_a_gerardo_con_distancia()
+
+func cambiar_estado(nuevo_estado: EstadoGuillermo) -> void:
+    if nuevo_estado != estado_actual:
+        estado_actual = nuevo_estado
+        actualizar_animacion()
+
+func seguir_a_gerardo_con_distancia() -> void:
+    var distancia := global_position.distance_to(gerardo.global_position)
+    if distancia < DISTANCIA_MINIMA_DE_GERARDO:
+        velocity = Vector2.ZERO
+    else:
+        var direccion := (gerardo.global_position - global_position).normalized()
+        velocity = direccion * VELOCIDAD_CAMINAR
+    move_and_slide()
+
+func actualizar_animacion():
+    match estado_actual:
+        EstadoGuillermo.NEUTRAL:
+            sprite.play("neutral")
+        EstadoGuillermo.SUFRIENDO:
+            sprite.play("sufriendo")
